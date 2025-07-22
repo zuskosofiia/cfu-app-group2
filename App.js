5acd126e20984488a5a0f890c7f769d5c4998034
unknown <dianochka.matla@gmail.com>
2025-07-22 16:19:31 +0300

    додала аватар

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image  } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Image style={{borderColor : 'black',
+            borderWidth: 5,
+            width: 120,
+            height: 120,
+            borderBottomRightRadius: 60,
+            borderBottomLeftRadius: 60,
+            borderTopRightRadius: 60,
+            borderTopLeftRadius: 60,
+            margin: 30}}
+            source={}
+          
     </View>
   );
 }
